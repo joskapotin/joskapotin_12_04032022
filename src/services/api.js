828827bbf82ed8b9ec9771806da8e403f90055de
joska <jpotin@gmail.com>
2022-06-27 21:40:57 +0200

    deploy ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import useFetch from "../hooks/useFetch/useFetch"
 
-const getUserMain = id => useFetch({ url: `/mock/userMainDataID${id}.json` })
-const getUserActivity = id => useFetch({ url: `/mock/userActivityID${id}.json` })
-const getUserAverageSession = id => useFetch({ url: `/mock/userAverageSessionID${id}.json` })
-const getUserPerformance = id => useFetch({ url: `/mock/userPerformanceID${id}.json` })
+/**
+ * Mock API
+ */
+const getUserMain = id => useFetch({ url: `./mock/userMainDataID${id}.json` })
+const getUserActivity = id => useFetch({ url: `./mock/userActivityID${id}.json` })
+const getUserAverageSession = id => useFetch({ url: `./mock/userAverageSessionID${id}.json` })
+const getUserPerformance = id => useFetch({ url: `./mock/userPerformanceID${id}.json` })
 
 export default {
   getUserMain,
