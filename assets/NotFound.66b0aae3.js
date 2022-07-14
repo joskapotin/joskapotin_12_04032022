5042579b0b07a2dee73109487f44160a8c52e19d
joska <jpotin@gmail.com>
2022-07-14 19:44:51 +0200

    Updates

--- assets/NotFound.66b0aae3.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index.dc8dedea.js";function t(){return o("h1",{className:"main__title",children:"Error 404 - Page not found"})}export{t as default};
---
+++ assets/NotFound.dfb13415.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.88b0209c.js";function t(){return o("h1",{className:"main__title",children:"Error 404 - Page not found"})}export{t as default};
