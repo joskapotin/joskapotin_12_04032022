88f48050ff0c8536eb5d02b57c3718a8050c3a70
joska <jpotin@gmail.com>
2022-07-14 19:43:51 +0200

    path fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/joskapotin_12_04032022/",
   plugins: [react(), tsconfigPaths()],
 })
